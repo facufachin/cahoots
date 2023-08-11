e862657ff6b683becd48c65f20886b3ea72344a1
facufachin <facusangui1976@gmail.com>
2023-08-11 14:15:25 -0300

    Updates

---
+++ precache-manifest.420583eab22947db14116cd870f79dea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6c8bd5d1e2665ec905b8161f5fc0b7",
+    "url": "/cahoots/index.html"
+  },
+  {
+    "revision": "7f938295be3047e05aa4",
+    "url": "/cahoots/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "260d40525825cba09ce7",
+    "url": "/cahoots/static/css/main.aa7cdbea.chunk.css"
+  },
+  {
+    "revision": "7f938295be3047e05aa4",
+    "url": "/cahoots/static/js/2.73a2a140.chunk.js"
+  },
+  {
+    "revision": "9e5af460d6e972fc25972720b94457e4",
+    "url": "/cahoots/static/js/2.73a2a140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260d40525825cba09ce7",
+    "url": "/cahoots/static/js/main.b1310e8f.chunk.js"
+  },
+  {
+    "revision": "a5827c46a2795f7f7e62",
+    "url": "/cahoots/static/js/runtime-main.285b0425.js"
+  }
+]);
